982630024c2894a6c724bec75eaba516f630e21c
theforestvn88 <theforestvn88@gmail.com>
2024-04-18 15:29:21 +0700

    improve test-helper

--- test_helper/assertion.rb
+++ test_helper/assertion.rb
@@ -5,10 +5,22 @@
 def assert_equal(reality, expected)
     if reality == expected
         @count_success += 1
-        @result << "#{@result.size+1}. OK\n\n"
+        @result << "#{@result.size+1}. OK\n------------------------------------------------------------------------------------\n------------------------------------------------------------------------------------\n"
     else
         @count_failed += 1
-        @result << "#{@result.size+1}. FAILED receive: #{reality}\n\n"
+        @result << <<~TEXT
+#{@result.size+1}. FAILED
+    
+Expect:
+    #{reality}
+
+To Equal:
+    #{expected}
+
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+
+TEXT
     end
 end
 
@@ -22,10 +34,31 @@ def assert_raises(expected, &block)
     rescue => error
         if error.class == expected.class && error.message == expected.message
             @count_success += 1
-            @result << "#{@result.size+1}. OK\n\n"
+            @result << "#{@result.size+1}. OK\n------------------------------------------------------------------------------------\n------------------------------------------------------------------------------------\n"
         else
             @count_failed += 1
-            @result << "#{@result.size+1}. FAILED get: #{error}\n\n"
+            @result << <<~TEXT
+#{@result.size+1}. FAILED 
+
+Expect Error: #{expected}
+
+Receive Error: #{error}
+
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+TEXT
+        end
+    ensure
+        if error.nil?
+            @count_failed += 1
+            @result << <<~TEXT
+#{@result.size+1}. FAILED 
+
+Expect to raise Error but nothing happen
+
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+TEXT
         end
     end
 end
@@ -33,5 +66,11 @@ end
 def run(testcases)
     eval(testcases)
 
-    "#{@result.join('')}\n------------------------------------------\nOK: #{@count_success}, FAILED: #{@count_failed}\n\n"
+    output = "#{@result.join('')}\n\nOK: #{@count_success}, FAILED: #{@count_failed}\n\n"
+    
+    @count_success = 0
+    @count_failed = 0
+    @result = []
+
+    output
 end
